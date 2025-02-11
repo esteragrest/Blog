e07ef5e5d3c8d7fbdb40ae3f230ca5390e7ed24a
Дарья Провальская <kaamomee@gmail.com>
2025-02-11 17:39:10 +0300

    Настроить проект

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+	text-align: center;
+`;
+
+export const App = () => {
+	return (
+		<Div>
+			<i className="fa fa-camera-retro"></i>
+			<i className="fa fa-podcast"></i>
+			<div>456</div>
+		</Div>
+	);
+};
