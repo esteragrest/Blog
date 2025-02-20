c0526451e56ebff3366dd45cd2c93e08cd03c318
Дарья Провальская <kaamomee@gmail.com>
2025-02-20 16:47:51 +0300

    Создан футер

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const FooterContainer = ({ className }) => {
+	const [city, setCity] = useState('');
+	const [temperature, setTemperature] = useState('');
+	const [weather, setWeather] = useState('');
+
+	useEffect(() => {
+		fetch(
+			'https://api.openweathermap.org/data/3.0/onecall?q=oscowM&lang=ru&appid={2c597e55be20453d7b699e01d126b3d6}',
+		)
+			.then((res) => res.json())
+			.then(({ name, main, weather }) => {
+				setCity(name);
+				setTemperature(Math.round(main.temp));
+				setWeather(weather[0].description);
+			});
+	}, []);
+
+	return (
+		<div className={className}>
+			<div>
+				<div>Блог веб-разработчика</div>
+				<div>web@developer.ru</div>
+			</div>
+			<div>
+				<div>
+					{city},{' '}
+					{new Date().toLocaleString('ru', { day: 'numeric', month: 'long' })}
+				</div>
+				<div>
+					{temperature} градусов, {weather}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export const Footer = styled(FooterContainer)`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	height: 120px;
+	width: 1000px;
+	height: 120px;
+	font-weight: bold;
+	background-color: #fff;
+	padding: 20px 40px;
+	box-shadow: 0 2px 17px #000;
+`;
+
+FooterContainer.propTypes = {
+	className: PropTypes.any,
+};
