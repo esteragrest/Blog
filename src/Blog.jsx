170b7c4ca6ebe37b9d5271f0b1b1ffcd69de326f
Дарья Провальская <kaamomee@gmail.com>
2025-03-01 18:30:23 +0300

    Реализована страница регистрации

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import { Header, Footer } from './components';
 import styled from 'styled-components';
-import { Authorization } from './pages';
+import { Authorization, Registration } from './pages';
 
 const AppColumn = styled.div`
 	display: flex;
@@ -25,7 +25,7 @@ export const Blog = () => {
 				<Routes>
 					<Route path="/" element={<div>Главная страница</div>} />
 					<Route path="/login" element={<Authorization />} />
-					<Route path="/register" element={<div>Страница регистрации</div>} />
+					<Route path="/register" element={<Registration />} />
 					<Route path="/users" element={<div>Пользователи</div>} />
 					<Route path="/post/:postId" element={<div>Статья</div>} />
 					<Route path="/post" element={<div>Новая статья</div>} />
--- src/pages/registration/Registration.jsx
+++ src/pages/registration/Registration.jsx
@@ -6,14 +6,14 @@ import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Input, Button, H2, AuthFormError } from '../../components';
 import styled from 'styled-components';
-import { Link, Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { setUser } from '../../actions/';
 import { selectUserRole } from '../../selectors';
 import { ROLE } from '../../constans';
 import { useResetForm } from '../../hooks/use-reset-form';
 
-const authFormShema = yup.object().shape({
+const regFormShema = yup.object().shape({
 	login: yup
 		.string()
 		.required('Заполните поле логин')
@@ -29,16 +29,13 @@ const authFormShema = yup.object().shape({
 		)
 		.min(6, 'Неверно заполнен пароль. Должно быть миинимум 6 символов')
 		.max(30, 'Неверно заполнен пароль. Должно быть максимум 30 символов'),
+	passcheck: yup
+		.string()
+		.required('Повторите пароль')
+		.oneOf([yup.ref('password'), null], 'Повтор пароля не совпадает'),
 });
 
-const StyledLink = styled(Link)`
-	text-align: center;
-	text-decoration: underline;
-	margin: 20px 0;
-	font-size: 18px;
-`;
-
-const AuthorizationContainer = ({ className }) => {
+const RegistrationContainer = ({ className }) => {
 	const {
 		register,
 		reset,
@@ -49,7 +46,7 @@ const AuthorizationContainer = ({ className }) => {
 			login: '',
 			password: '',
 		},
-		resolver: yupResolver(authFormShema),
+		resolver: yupResolver(regFormShema),
 	});
 
 	const dispath = useDispatch();
@@ -61,7 +58,7 @@ const AuthorizationContainer = ({ className }) => {
 	const [serverError, setServerError] = useState(null);
 
 	const onSubmit = ({ login, password }) => {
-		server.authorize(login, password).then(({ error, res }) => {
+		server.register(login, password).then(({ error, res }) => {
 			if (error) {
 				setServerError(`Ошибка запроса: ${error}`);
 				return;
@@ -71,7 +68,8 @@ const AuthorizationContainer = ({ className }) => {
 		});
 	};
 
-	const formError = errors?.login?.message || errors?.password?.message;
+	const formError =
+		errors?.login?.message || errors?.password?.message || errors?.passcheck?.message;
 
 	const errorMessage = formError || serverError;
 
@@ -81,7 +79,7 @@ const AuthorizationContainer = ({ className }) => {
 
 	return (
 		<div className={className}>
-			<H2>Авторизация</H2>
+			<H2>Регистрация</H2>
 			<form onSubmit={handleSubmit(onSubmit)}>
 				<Input
 					type="text"
@@ -93,17 +91,21 @@ const AuthorizationContainer = ({ className }) => {
 					placeholder="Пароль..."
 					{...register('password', { onChange: () => setServerError(null) })}
 				/>
+				<Input
+					type="password"
+					placeholder="Повторите пароль..."
+					{...register('passcheck', { onChange: () => setServerError(null) })}
+				/>
 				<Button type="submit" disabled={!!formError}>
-					Авторизоваться
+					Зарегистрироваться
 				</Button>
 				{errorMessage && <AuthFormError>{errorMessage}</AuthFormError>}
-				<StyledLink to="/register">Регистрация</StyledLink>
 			</form>
 		</div>
 	);
 };
 
-export const Authorization = styled(AuthorizationContainer)`
+export const Registration = styled(RegistrationContainer)`
 	display: flex;
 	justify-content: center;
 	align-items: center;
@@ -118,6 +120,6 @@ export const Authorization = styled(AuthorizationContainer)`
 	}
 `;
 
-AuthorizationContainer.propTypes = {
+RegistrationContainer.propTypes = {
 	className: PropTypes.string,
 };
